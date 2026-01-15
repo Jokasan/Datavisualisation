3e224905bd30382c8eed6844e234d0f8c23b855e
Jokasan <indreitennils@gmail.com>
2026-01-15 22:00:50 +0000

    loaded in tidy tuesday data

--- TidyTuesday/languages_in_Africa.r
+++ TidyTuesday/languages_in_Africa.r
@@ -1,2 +1,11 @@
 https://www.dataviz-inspiration.com/ # A world of languages
 https://github.com/uRosConf/voronoiTreemap
+
+# Load in the data:
+tuesdata <- tidytuesdayR::tt_load('2026-01-13')
+
+africa <- tuesdata$africa
+
+# Initial idea, some form of Sankey diagram 
+# WIth highights and a small map on the 
+# side.
