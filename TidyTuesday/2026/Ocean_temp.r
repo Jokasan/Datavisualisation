62038e9ea52b3c325346cd56d9586a19d34dd6a2
Nils <nila@Nilss-MacBook-Air.local>
2026-04-10 11:34:23 +0200

    added typo fix for Nova Scotia plot

--- TidyTuesday/2026/Ocean_temp.r
+++ TidyTuesday/2026/Ocean_temp.r
@@ -78,7 +78,7 @@ heatmap <- ggplot(
 
   labs(
     title = "4 Years Beneath the Surface",
-    subtitle = "Daily mean ocean temperature at Birchy Head, Nova Scotia, by ocean sensor depth.\nWarmer tempertaures seep to ocean depths and are lasting longer into the year.",
+    subtitle = "Daily mean ocean temperature at Birchy Head, Nova Scotia, by ocean sensor depth.\nWarmer temperatures seep to ocean depths and are lasting longer into the year.",
     x = NULL,
     y = NULL,
     caption = "Data: Centre for Marine Applied Research · Coastal Monitoring Program | #TidyTuesday 2026-03-31"
